b30ff46c56fd9100b47a9883ff9fd70ae1fef1aa
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2023-03-19 06:37:09 +0000

    Fix output paths and Caddyfile

--- make-requests.sh
+++ make-requests.sh
@@ -2,13 +2,14 @@
 
 set -eu
 
+cd "$(dirname "$0")"
 expected_data='#done.test.1234567890'
 
 for i in {1..1000}; do
     curl -sS "https://$1/api/run/test" \
         --http1.1 \
-        --data-urlencode 'id=1234567890' > out.txt
-    if ! grep -Fq "$expected_data" out.txt; then
+        --data-urlencode 'id=1234567890' > data/out.txt
+    if ! grep -Fq "$expected_data" data/out.txt; then
         echo "Failed after $i requests"
         exit 1
     fi
